ca2aac2f607880f76b3138b2e3a2f4f7ea1b3c7e
Renal Apriansyah <>
2019-12-07 23:08:27 +0700

    use serialize to prevent xss

--- src/server/renderer.tsx
+++ src/server/renderer.tsx
@@ -4,6 +4,7 @@ import { StaticRouter } from 'react-router-dom'
 import Routes from '../client/Routes'
 import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
+import serialize from 'serialize-javascript'
 
 export default (req: any, store: any) => {
   const content = renderToString(
@@ -24,7 +25,7 @@ export default (req: any, store: any) => {
     <body>
       <div id="app">${content}</div>
       <script>
-        window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+        window.INITIAL_STATE = ${serialize(store.getState())}
       </script>
       <script src="bundle.js"></script>
     </html>
