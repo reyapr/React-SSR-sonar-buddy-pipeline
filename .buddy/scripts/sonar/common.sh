604493352fb7a4f87693c3638461521075735957
Renal Apriansyah <renal.apr@gmail.com>
2022-04-25 05:49:37 +0700

    update color

--- .buddy/scripts/sonar/common.sh
+++ .buddy/scripts/sonar/common.sh
@@ -4,12 +4,12 @@
 set -e
 set -o pipefail
 
-gray="\\e[37m"
-blue="\\e[36m"
-red="\\e[31m"
-yellow="\\e[33m"
-green="\\e[32m"
-reset="\\e[0m"
+gray="\e[37m"
+blue="\e[36m"
+red="\e[31m"
+yellow="\e[33m"
+green="\e[32m"
+reset="\e[0m"
 
 info() { echo -e "${blue}INFO: $*${reset}"; }
 error() { echo -e "${red}ERROR: $*${reset}"; }
